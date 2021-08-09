094d8dcd6c2bed2c04c3cf562a33f992bace0282
bahramali <bahramali.az@gmail.com>
2021-08-09 07:56:14 +0200

    - change the name of module common to javaBasicConcepts - add a new class for first homework

---
+++ javaBasicConcepts/src/ir/java/homework/FunctionHomework.java
@@ -0,0 +1,25 @@
+package ir.java.homework;
+
+public class FunctionHomework {
+
+    /*
+        create a method isPrime(int num)
+        It should return true or false
+        You need to implement the method to recognize the number if it is prime or not
+     */
+
+
+
+    /*
+    * Write the method again with while, do-wile and for
+    * */
+
+
+    /*
+    * Write a method to take a number as a parameter and return all prime numbers before then
+    * You can call it printPrimes(int num)
+    * */
+
+
+
+}
