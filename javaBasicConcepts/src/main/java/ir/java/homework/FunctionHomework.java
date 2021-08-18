584be6dd98f551639cd752c2431249662f3672b2
Reza Shoohani <rezashoohani6@gmail.com>
2021-08-18 16:37:42 +0430

    Working on switch case and loop

--- javaBasicConcepts/src/main/java/ir/java/homework/FunctionHomework.java
+++ javaBasicConcepts/src/main/java/ir/java/homework/FunctionHomework.java
@@ -40,5 +40,12 @@ public class FunctionHomework {
         return result;
     }
 
-
+    public static void main(String[] args) {
+        for (int a = 1; a <= 10; a++) {
+            for (int b = 1; b <= 10; b++) {
+                System.out.print(String.valueOf(a+"*"+b+"="+b * a + "\t"));
+            }
+            System.out.println();
+        }
+    }
 }
--- javaBasicConcepts/test/java/ir/java/homework/FunctionHomeworkTest.java
+++ javaBasicConcepts/test/java/ir/java/homework/FunctionHomeworkTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import javax.swing.plaf.IconUIResource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FunctionHomeworkTest {
@@ -26,4 +28,91 @@ class FunctionHomeworkTest {
         assertTrue(actual);
     }
 
+    @Test
+    void test_basic_operation() {
+        long a = 12;
+        int b = 66;
+        int c = 66;
+        char myChar = 'A';
+        b = (int) a;
+        assertEquals(12, b);
+
+        System.out.println(myChar);
+        b = myChar;
+
+        assertEquals(65, b);
+        myChar = (char) c;
+        System.out.println(myChar);
+    }
+
+    @Test
+    void test_break_continue() {
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) {
+                System.out.print("[ ] ");
+                continue;
+            }
+            System.out.print("[" + i + "] ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) {
+                System.out.print("[ ] ");
+                break;
+            }
+            System.out.print("[" + i + "] ");
+        }
+        System.out.println();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 5, 6, 8, 2, 14, 6})
+
+    public void test_switch(int num) {
+
+
+        switch (num) {
+            case 1:
+                System.out.println("sunday");
+                break;
+            case 2:
+                System.out.println("monday");
+                break;
+            case 3:
+                System.out.println("tuesday");
+                break;
+            case 4:
+                System.out.println("wednesday");
+                break;
+            case 5:
+                System.out.println("thursday");
+                break;
+            case 6:
+                System.out.println("friday");
+                break;
+            case 7:
+                System.out.println("saturday");
+                break;
+
+            default:
+                System.out.println("THe Num You Add , Is Fuels");
+
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2})
+    public void multiplication(int a, int b) {
+
+        for (a = 1; a <= 10; a++) {
+            for (b = 1; b <= 10; b++) {
+                System.out.print(String.valueOf(a * b));
+            }
+            System.out.println();
+        }
+    }
+
+
 }
+
